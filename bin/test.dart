53fa2b6cbe2170155a3a94fa94060c025f9ec603
abhishek shrivastava <abhishekshrivastava78@gmail.com>
2019-11-18 12:00:30 +0530

    test case written

--- bin/test.dart
+++ bin/test.dart
@@ -7,8 +7,16 @@ void main() {
     expect(game1.play("orange"),true);
     expect(game1.play("eagle"),true);
     expect(game1.play("ear"),true);
-    expect(game1.play("camel"),false);
+    expect(game1.play("camel"),false); //camel does not match with ear
     game1.restart();
     expect(game1.play("hello"),true);
+    expect(game1.play("hello"),false); // duplicate word in list
+    game1.restart();
+    expect(game1.play("hello"),true);
+    expect(game1.play(""),false); // check for invalid word
+    game1.restart();
+    expect(game1.play("hello"),true);
+    expect(game1.play("Orange"),true); // check for lower /upper case
+    expect(game1.play("Elephant"),true);
   });
 }
